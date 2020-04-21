e96e0ed2d1351f9d7d2a93964b02cb221c085544
Dillon Reedy <dillon.reedy123@gmail.com>
2020-04-21 14:15:50 -0400

    Adding production build to final product

---
+++ build/precache-manifest.41e7f56cbfa8a83c7386811129c49441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080fb4f12e4bc498294fb63c7f047ea1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc990d4b90da52e8e41b",
+    "url": "/static/css/main.b0495a33.chunk.css"
+  },
+  {
+    "revision": "3300f7edfb2fc84149bd",
+    "url": "/static/js/2.303845ed.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.303845ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc990d4b90da52e8e41b",
+    "url": "/static/js/main.2235425f.chunk.js"
+  },
+  {
+    "revision": "f977e3d0490a6b3ba396",
+    "url": "/static/js/runtime-main.4bc69063.js"
+  }
+]);
